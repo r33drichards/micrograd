2e3c52fa83ef4a4031515523c0df34014f8278bf
rw <rw@jjk.is>
2024-05-13 14:39:40 -0700

    try this

--- torchrl.nix
+++ torchrl.nix
@@ -1,20 +1,14 @@
-{ lib, buildPythonPackage, fetchPypi, python }:
-https://files.pythonhosted.org/packages/4d/41/3cd8607a4917a8c4859ec28dfb9fcc79c419c12a066bcacb0934a95282ed/torchrl-0.3.1-cp311-cp311-manylinux1_x86_64.whl
+{ lib, buildPythonPackage, fetchurl, python }:
+
 buildPythonPackage rec {
   pname = "torchrl";
   version = "0.3.1";
   format = "wheel";
 
-
-  src = fetchPypi rec {
-    inherit pname version format;
-    sha256 = ""; # TODO
-    dist = "cp311";
-    python = "cp311";
-    #abi = "none";
-    platform = "manylinux1_x86_64";
+  src = fetchurl {
+    url = "https://files.pythonhosted.org/packages/4d/41/3cd8607a4917a8c4859ec28dfb9fcc79c419c12a066bcacb0934a95282ed/torchrl-0.3.1-cp311-cp311-manylinux1_x86_64.whl";
+    sha256 = ""; # TODO: Add the sha256 hash for the wheel file
   };
-
 
   propagatedBuildInputs = [
     python.pkgs.torch
